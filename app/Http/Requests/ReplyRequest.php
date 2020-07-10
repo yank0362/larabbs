cc1bcac677bcbad2e0bf544c0ef7271f56c46b74
Damon <yank0362@qq.com>
2020-07-10 21:52:56 +0800

    用户可以发表评论

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -2,34 +2,18 @@
 
 namespace App\Http\Requests;
 
+/**
+ * @property String content
+ * Class ReplyRequest
+ * @package App\Http\Requests
+ */
 class ReplyRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            }
-        }
+        return [
+            'content' => 'required|min:2',
+        ];
     }
 
     public function messages()
