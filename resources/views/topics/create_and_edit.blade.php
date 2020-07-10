4173f1bd314ce87aaac8371e72549ab1cc2ae9f6
Damon <yank0362@qq.com>
2020-07-10 14:00:32 +0800

    用户可以上传话题图片

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -60,6 +60,8 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
 @stop
 
+
+
 @section('scripts')
     <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
@@ -70,6 +72,16 @@
         $(document).ready(function() {
             var editor = new Simditor({
                 textarea: $('#editor'),
+                upload: {
+                    url: '{{ route('topics.upload_image') }}',
+                    params: {
+                        _token: '{{ csrf_token() }}'
+                    },
+                    fileKey: 'upload_file',
+                    connectionCount: 3,
+                    leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+                },
+                pasteImage: true,
             });
         });
     </script>
